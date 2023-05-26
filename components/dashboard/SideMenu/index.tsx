7cc5c23d37e9562b6b2205e59efa38cc43e04dd6
emre1n <emre1n@protonmail.com>
2023-05-26 18:33:02 +0300

    add icon to sidemenu

--- components/dashboard/SideMenu/index.tsx
+++ components/dashboard/SideMenu/index.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
 
+import { icons } from 'react-icons';
+import { BsPalette } from 'react-icons/bs';
+
 const SideMenu = () => {
   return (
-    <div className="h-screen w-28 border-r border-gray-text pr-2 text-gray-text hover:text-black">
-      <p className="text-center duration-500 hover:border-b hover:border-b-black">
+    <div className="h-screen w-32 flex-col border-r border-gray-text pr-2 text-gray-text">
+      <span className="flex gap-2 p-2 text-center duration-500 hover:border-b hover:border-b-black hover:text-black">
+        <BsPalette className="inline place-self-center" />
         <Link href="/dashboard/artworks">Artworks</Link>
-      </p>
+      </span>
     </div>
   );
 };
