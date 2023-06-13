34ba5da4c900ce670681f5514ee3909e768aae9c
emre1n <emre1n@protonmail.com>
2023-06-13 15:42:58 +0300

    remove console.log from getArtworks

--- app/actions/getArtworks.ts
+++ app/actions/getArtworks.ts
@@ -3,7 +3,6 @@ import prisma from '@/libs/prismadb';
 export default async function getArtworks() {
   try {
     const data = await prisma.artworks.findMany();
-    console.log('data prisma', data);
     return data;
   } catch (error: any) {
     console.log('database failed');
