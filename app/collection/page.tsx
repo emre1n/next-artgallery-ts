aa237fc43dcdb9f4be1cc13f3dd0b2de7f3aa099
emre1n <emre1n@protonmail.com>
2023-06-13 15:04:17 +0300

    remove comments collection page

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -1,14 +1,8 @@
 import ArtworkList from '@/components/collection/ArtworkList';
-import { getPrismaData } from '@/libs/db-service';
-import { prismaGenerate } from '@/prisma/create';
-import { useGalleryStore } from '@/stores/useGalleryStore';
 
 import getArtworks from '../actions/getArtworks';
 
-// import { useGalleryStore } from '@/stores/store';
-
 async function Collection() {
-  // const artworks = useGalleryStore(store => store.artworks);
   const artworks = await getArtworks();
 
   return (
